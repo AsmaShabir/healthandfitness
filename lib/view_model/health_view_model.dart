1161e2a884393b268b3753db267c85a0cccdd061
Uzair-Qaiser <iamuzairqaiser@gmail.com>
2024-10-02 11:36:39 +0500

    commit & push by uzair

--- lib/view_model/health_view_model.dart
+++ lib/view_model/health_view_model.dart
@@ -8,55 +8,48 @@ class HealthViewModel with ChangeNotifier {
   var health ;
   HealthData? _healthData;
   HealthData? get healthData => _healthData;
-  healthFunc(){
+  healthFunc()async{
     Health().configure();
+    // Initialize the HealthFactory
 
+    // Define the types to get
+    var types = [
+      HealthDataType.STEPS,
+      HealthDataType.BLOOD_GLUCOSE,
+    ];
 
+    // Request access to the data types before reading them
+    bool requested = await health.requestAuthorization(types);
 
+    var now = DateTime.now();
 
-    Future<void> getData() async {
-      // Initialize the HealthFactory
+    // Fetch health data from the last 24 hours
+    List<HealthDataPoint> healthData = await health.getHealthDataFromTypes(
+        now.subtract(Duration(days: 1)), now, types);
 
-      // Define the types to get
-      var types = [
+    // Request permissions to write steps and blood glucose
+    var permissions = [
+      HealthDataAccess.READ_WRITE,
+      HealthDataAccess.READ_WRITE
+    ];
+    await health.requestAuthorization(types, permissions: permissions);
+
+    // Write steps and blood glucose
+    bool success = await health.writeHealthData(10, HealthDataType.STEPS, now, now);
+    success &= await health.writeHealthData(3.1, HealthDataType.BLOOD_GLUCOSE, now, now);
+
+    // Manual recording method (optional, for Android and iOS)
+    success &= await health.writeHealthData(
+        10,
         HealthDataType.STEPS,
-        HealthDataType.BLOOD_GLUCOSE,
-      ];
+        now,
+        now,
+        recordingMethod: RecordingMethod.manual);
 
-      // Request access to the data types before reading them
-      bool requested = await health.requestAuthorization(types);
-
-      var now = DateTime.now();
-
-      // Fetch health data from the last 24 hours
-      List<HealthDataPoint> healthData = await health.getHealthDataFromTypes(
-          now.subtract(Duration(days: 1)), now, types);
-
-      // Request permissions to write steps and blood glucose
-      var permissions = [
-        HealthDataAccess.READ_WRITE,
-        HealthDataAccess.READ_WRITE
-      ];
-      await health.requestAuthorization(types, permissions: permissions);
-
-      // Write steps and blood glucose
-      bool success = await health.writeHealthData(10, HealthDataType.STEPS, now, now);
-      success &= await health.writeHealthData(3.1, HealthDataType.BLOOD_GLUCOSE, now, now);
-
-      // Manual recording method (optional, for Android and iOS)
-      success &= await health.writeHealthData(
-          10,
-          HealthDataType.STEPS,
-          now,
-          now,
-          recordingMethod: RecordingMethod.manual);
-
-      // Get the number of steps for today
-      var midnight = DateTime(now.year, now.month, now.day);
-      int? steps = await health.getTotalStepsInInterval(midnight, now);
-
-    }
-    notifyListeners();
+    // Get the number of steps for today
+    var midnight = DateTime(now.year, now.month, now.day);
+    int? steps = await health.getTotalStepsInInterval(midnight, now);
+notifyListeners();
   }
 }
 
