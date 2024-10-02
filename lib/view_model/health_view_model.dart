ce9ddd14619b52db97f82346deb906d1b8de2fd4
Uzair-Qaiser <iamuzairqaiser@gmail.com>
2024-10-02 11:50:02 +0500

    commit & push by uzair

--- lib/view_model/health_view_model.dart
+++ lib/view_model/health_view_model.dart
@@ -53,4 +53,44 @@ class HealthViewModel with ChangeNotifier {
   }
 }
 
-
+//
+//
+// // configure the health plugin before use.
+// Health().configure();
+//
+//
+// // define the types to get
+// var types = [
+//   HealthDataType.STEPS,
+//   HealthDataType.BLOOD_GLUCOSE,
+// ];
+//
+// // requesting access to the data types before reading them
+// bool requested = await Health().requestAuthorization(types);
+//
+// var now = DateTime.now();
+//
+// // fetch health data from the last 24 hours
+// List<HealthDataPoint> healthData = await Health().getHealthDataFromTypes(
+// now.subtract(Duration(days: 1)), now, types);
+//
+// // request permissions to write steps and blood glucose
+// types = [HealthDataType.STEPS, HealthDataType.BLOOD_GLUCOSE];
+// var permissions = [
+// HealthDataAccess.READ_WRITE,
+// HealthDataAccess.READ_WRITE
+// ];
+// await Health().requestAuthorization(types, permissions: permissions);
+//
+// // write steps and blood glucose
+// bool success = await Health().writeHealthData(10, HealthDataType.STEPS, now, now);
+// success = await Health().writeHealthData(3.1, HealthDataType.BLOOD_GLUCOSE, now, now);
+//
+// // you can also specify the recording method to store in the metadata (default is RecordingMethod.automatic)
+// // on iOS only `RecordingMethod.automatic` and `RecordingMethod.manual` are supported
+// // Android additionally supports `RecordingMethod.active` and `RecordingMethod.unknown`
+// success &= await Health().writeHealthData(10, HealthDataType.STEPS, now, now, recordingMethod: RecordingMethod.manual);
+//
+// // get the number of steps for today
+// var midnight = DateTime(now.year, now.month, now.day);
+// int? steps = await Health().getTotalStepsInInterval(midnight, now);
