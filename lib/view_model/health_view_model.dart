340cca053e9d1503c1817e01cdbbebc22267153b
Uzair-Qaiser <iamuzairqaiser@gmail.com>
2024-10-02 11:49:04 +0500

    commit & push by uzair

--- lib/view_model/health_view_model.dart
+++ lib/view_model/health_view_model.dart
@@ -5,8 +5,7 @@ import '../model/health_model.dart';
 
 
 class HealthViewModel with ChangeNotifier {
-  Health? _health ;
-  Health? get health=>_health;
+  var health ;
   HealthData? _healthData;
   HealthData? get healthData => _healthData;
   healthFunc()async{
@@ -20,20 +19,20 @@ class HealthViewModel with ChangeNotifier {
     ];
 
     // Request access to the data types before reading them
-    bool? requested = await health?.requestAuthorization(types);
+    bool requested = await health.requestAuthorization(types);
 
     var now = DateTime.now();
 
     // Fetch health data from the last 24 hours
-    List<HealthDataPoint>? healthData = await health?.getHealthDataFromTypes(
-        now.subtract(Duration(days: 1)), now, types, types: [], startTime: null, endTime: null);
+    List<HealthDataPoint> healthData = await health.getHealthDataFromTypes(
+        now.subtract(Duration(days: 1)), now, types);
 
     // Request permissions to write steps and blood glucose
     var permissions = [
       HealthDataAccess.READ_WRITE,
       HealthDataAccess.READ_WRITE
     ];
-    await health?.requestAuthorization(types, permissions: permissions);
+    await health.requestAuthorization(types, permissions: permissions);
 
     // Write steps and blood glucose
     bool success = await health.writeHealthData(10, HealthDataType.STEPS, now, now);
@@ -49,8 +48,8 @@ class HealthViewModel with ChangeNotifier {
 
     // Get the number of steps for today
     var midnight = DateTime(now.year, now.month, now.day);
-    int? steps = await health?.getTotalStepsInInterval(midnight, now);
-notifyListeners();
+    int? steps = await health.getTotalStepsInInterval(midnight, now);
+    notifyListeners();
   }
 }
 
